45668b54d09eb84b411c79f24424ebd4df0b3a29
Kyle Reczek <kyle@banyango.com>
2019-06-21 15:27:42 -0600

    Project setup with basic webpack working

---
+++ src/components/sidebar.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Sidebar extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {entities:[]}
+    }
+    render() {
+
+    }
+}
+
+export default Sidebar;
